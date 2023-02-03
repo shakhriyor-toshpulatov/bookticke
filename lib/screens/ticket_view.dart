d95f1d5aa704aff6e7c9447b82133b2ca459c174
shakh <shakhriyor315@gmail.com>
2023-02-03 12:00:22 +0500

    ticket view. Apply decoration property to container.Add expanded widget and use flex as child property

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,5 +1,6 @@
 import 'package:booktickets/utils/app_layout.dart';
 import 'package:booktickets/utils/app_styles.dart';
+import 'package:booktickets/widgets/thick_container.dart';
 import 'package:flutter/material.dart';
 
 class TicketView extends StatelessWidget {
@@ -30,12 +31,57 @@ class TicketView extends StatelessWidget {
                     children: [
                       Text(
                         "TASHKENT",
-                        style: Styles.headLineStyle3.copyWith(color: Colors.white),
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.white),
                       ),
-                      const Spacer(),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      ThickContainer(),
+                      Expanded(
+                        child: Stack(children: [
+                          SizedBox(
+                            height: 24,
+                            child: LayoutBuilder(
+                              builder: (BuildContext context,
+                                  BoxConstraints constraints) {
+                                return Flex(
+                                  direction: Axis.horizontal,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: List.generate(
+                                    (constraints.constrainWidth() / 6).floor(),
+                                    (index) => const SizedBox(
+                                      width: 3,
+                                      height: 1,
+                                      child: DecoratedBox(
+                                        decoration:
+                                            BoxDecoration(color: Colors.white),
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                          Center(
+                            child: Transform.rotate(
+                              angle: 1.5,
+                              child: Icon(Icons.local_airport_rounded,
+                                  color: Colors.white),
+                            ),
+                          ),
+                        ]),
+                      ),
+                      ThickContainer(),
+                      Expanded(
+                        child: Container(),
+                      ),
                       Text(
                         "BUKHARA",
-                        style: Styles.headLineStyle3.copyWith(color: Colors.white),
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.white),
                       ),
                     ],
                   ),
