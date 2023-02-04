76d57ee5a6c0d4dc0dac6d7163c167e20aff3539
shakh <shakhriyor315@gmail.com>
2023-02-04 15:09:59 +0500

    search_screen and icon text widget part 1

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,85 @@
+import 'package:booktickets/widgets/icon_text_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import '../utils/app_layout.dart';
+import '../utils/app_styles.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: AppLayout.getWidth(20),
+          vertical: AppLayout.getHeight(20),
+        ),
+        children: [
+          Gap(AppLayout.getHeight(40)),
+          Text(
+            "What are\nyou looking for? ",
+            style: Styles.headLineStyle1.copyWith(
+              fontSize: AppLayout.getWidth(35),
+            ),
+          ),
+          Gap(AppLayout.getHeight(20)),
+          FittedBox(
+            child: Container(
+              padding: const EdgeInsets.all(3.5),
+              child: Row(
+                children: [
+                  /*
+                taxi drivers
+                 */
+                  Container(
+                    width: size.width * .44,
+                    padding:
+                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(AppLayout.getHeight(50)),
+                      ),
+                      color: Colors.white,
+                    ),
+                    child: Center(
+                      child: Text("Taxi"),
+                    ),
+                  ),
+                  /*
+                  the packages
+               */
+
+                  Container(
+                    width: size.width * .44,
+                    padding:
+                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.horizontal(
+                        right: Radius.circular(AppLayout.getHeight(50)),
+                      ),
+                      color: Colors.transparent,
+                    ),
+                    child: Center(
+                      child: Text("The Packages"),
+                    ),
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(AppLayout.getHeight(50)),
+                color: const Color(0xFFF4F6FD),
+              ),
+            ),
+          ),
+          Gap(AppLayout.getHeight(25)),
+          const AppIconText(icon: Icons.flight_takeoff_rounded, text: "Depature"),
+          Gap(AppLayout.getHeight(25)),
+          const AppIconText(icon: Icons.flight_land_rounded, text: "Arrival"),
+        ],
+      ),
+    );
+  }
+}
