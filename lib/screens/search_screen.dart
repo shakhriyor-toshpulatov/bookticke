824cfaeaa243dad42040ecdf2b8e6b7560c44674
shakh <shakhriyor315@gmail.com>
2023-02-04 17:25:22 +0500

    search screen part 2

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -98,7 +98,137 @@ class SearchScreen extends StatelessWidget {
             ),
           ),
           Gap(AppLayout.getHeight(40)),
-          const AppDoubleTextWidget(bigText: "Upcoming flight", smallText: "View all"),
+          const AppDoubleTextWidget(
+              bigText: "Upcoming flight", smallText: "View all"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: AppLayout.getHeight(400),
+                width: size.width * 0.42,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: AppLayout.getHeight(190),
+                      decoration: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.circular(AppLayout.getHeight(12)),
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage('assets/images/driver1.png'))),
+                    ),
+                    Gap(AppLayout.getHeight(12)),
+                    Text(
+                      "20% discount on the early booking of this flight. Don`t miss out this chance",
+                      style: Styles.headLineStyle2,
+                    ),
+                  ],
+                ),
+              ),
+              Column(children: [
+                Stack(
+                  children: [
+                    Container(
+                      width: size.width * 0.44,
+                      height: AppLayout.getHeight(200),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF3ABBBB),
+                        borderRadius: BorderRadius.circular(
+                          AppLayout.getHeight(18),
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        vertical: AppLayout.getHeight(15),
+                        horizontal: AppLayout.getHeight(15),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Discount\nfor survey",
+                            style: Styles.headLineStyle2.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Gap(AppLayout.getHeight(10)),
+                          Text(
+                            "Take the survey about our services and get discount",
+                            style: Styles.headLineStyle2.copyWith(
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      right: -45,
+                      top: -40,
+                      child: Container(
+                        padding: EdgeInsets.all(AppLayout.getHeight(30)),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border:
+                              Border.all(width: 18, color: Color(0xFF189999)),
+                          color: Colors.transparent,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Gap(AppLayout.getHeight(10)),
+                Container(
+                  width: size.width * 0.44,
+                  height: AppLayout.getHeight(210),
+                  padding: EdgeInsets.symmetric(
+                    vertical: AppLayout.getHeight(15),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      AppLayout.getHeight(18),
+                    ),
+                    color: const Color(0xFFEC6545),
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Take love",
+                        style: Styles.headLineStyle2.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Gap(AppLayout.getHeight(5)),
+                      RichText(
+                        text: const TextSpan(
+                          children: [
+                            TextSpan(
+                              text: '😘',
+                              style: TextStyle(fontSize: 38),
+                            ),
+                            TextSpan(
+                              text: '😍',
+                              style: TextStyle(fontSize: 50),
+                            ),
+                            TextSpan(
+                              text: '‍💻',
+                              style: TextStyle(fontSize: 38),
+                            ),
+                          ]
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ]),
+            ],
+          ),
         ],
       ),
     );
